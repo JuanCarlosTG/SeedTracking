9dda38c54b9f3cb86d396b3e6f077ac2c41909ef
JuanCarlosTG <tlaxqueno.guerrero.j.c@gmail.com>
2016-05-15 21:27:05 -0500

    search farmer

--- app/src/main/java/com/kreativeco/sysbioscience/farmers/Farmers.java
+++ app/src/main/java/com/kreativeco/sysbioscience/farmers/Farmers.java
@@ -3,6 +3,7 @@ package com.kreativeco.sysbioscience.farmers;
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.kreativeco.sysbioscience.R;
@@ -29,6 +31,9 @@ public class Farmers extends Fragment implements WebBridge.WebBridgeListener{
     View v;
 
     private RecyclerView rvFarmer;
+    private ImageButton btnSearch, btnCloseSearch;
+    private EditText txtSearch;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,6 +47,23 @@ public class Farmers extends Fragment implements WebBridge.WebBridgeListener{
         RecyclerView.LayoutManager rvLayoutManager = new LinearLayoutManager(getActivity().getBaseContext());
         rvFarmer.setLayoutManager(rvLayoutManager);
         ImageButton iBtnAddFarmer = (ImageButton) v.findViewById(R.id.i_btn_add_farmer);
+        btnSearch = (ImageButton) v.findViewById(R.id.search_farmer);
+        txtSearch = (EditText) v.findViewById(R.id.txt_search);
+        btnCloseSearch = (ImageButton) v.findViewById(R.id.btn_close_search);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchFarmer(v);
+            }
+        });
+
+        btnCloseSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFarmers();
+            }
+        });
 
         iBtnAddFarmer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,11 +72,7 @@ public class Farmers extends Fragment implements WebBridge.WebBridgeListener{
             }
         });
 
-        HashMap<String, Object> params = new HashMap<>();
-        params.put("token", User.getToken(getActivity()));
-        params.put("metodo", "consultarPorSubdistribuidor");
-        WebBridge.send("Agricultor.ashx?List", params, getActivity(), this);
-
+        loadFarmers();
         return v;
 
     }
@@ -87,6 +105,24 @@ public class Farmers extends Fragment implements WebBridge.WebBridgeListener{
 
     @Override
     public void onWebBridgeFailure(String url, String response) {
+
+    }
+
+    public void searchFarmer(View view){
+
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("token", User.getToken(getActivity()));
+        params.put("rfc", txtSearch.getText().toString());
+        params.put("metodo", "consultarPorRFC");
+        WebBridge.send("Agricultor.ashx?List", params, getActivity(), this);
+    }
+
+
+    public void loadFarmers(){
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("token", User.getToken(getActivity()));
+        params.put("metodo", "consultarPorSubdistribuidor");
+        WebBridge.send("Agricultor.ashx?List", params, getActivity(), this);
 
     }
 }
